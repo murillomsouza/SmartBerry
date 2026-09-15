a179f818c2cbf98096729227440c223231a0fa18
murillomsouza <murillosouza997@gmail.com>
2026-09-15 09:37:45 -0300

    Primeiro commit: configurações iniciais API SmartBerry usando Java + SpringBoot

---
+++ src/test/java/br/com/smartberry/SmartberryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smartberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartberryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
